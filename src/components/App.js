ed476c9f96b48c5f8069f04101df955674bd3b20
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-05-07 13:01:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
-//code here 
+//code here 4
+  const[isBlue,setIsBlue]= useState(false);
+  const toggleColor=()=>{
+    setIsBlue(!isBlue);
+  };
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={isBlue?'blue':'red'} >Newton School</p>
+      <button id='button' onClick={toggleColor}>Change Style</button>
     </div>
   )
 }
